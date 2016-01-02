bedb38fef56c22035e552dfcc33fa56037f77989
vidya bhushan <vbhushan1986@gmail.com>
2016-01-02 17:09:45 +0530

    Signed-off-by: vidya bhushan <vbhushan1986@gmail.com>

---
+++ invite4sure-dataaccess-api/src/main/java/com/invite4sure/login/service/ILoginService.java
@@ -0,0 +1,13 @@
+/**
+ * ILoginService.java
+ */
+package com.invite4sure.login.service;
+
+/**
+ * @author vidyabhushan
+ *
+ */
+public interface ILoginService {
+
+	public boolean isValidUser(String usrName, String password);
+}
---
+++ invite4sure-dataaccess-impl/src/main/java/com/invite4sure/login/service/impl/LoginServiceImpl.java
@@ -0,0 +1,12 @@
+/**
+ * LoginServiceImpl.java
+ */
+package com.invite4sure.login.service.impl;
+
+/**
+ * @author vidyabhushan
+ *
+ */
+public class LoginServiceImpl {
+
+}
